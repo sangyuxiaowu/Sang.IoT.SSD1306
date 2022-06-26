c1c53881efde3cd22094a4799139d395ca204ed0
sangsq <sang93@qq.com>
2022-06-26 14:49:42 +0800

    添加项目文件。

---
+++ Sang.IoT.SSD1306/SSD1306_128_64.cs
@@ -0,0 +1,52 @@
+﻿namespace Sang.SSD1306
+{
+    internal class SSD1306_128_64 : SSD1306_Base
+    {
+        /// <summary>
+        /// SSD1306_128_64
+        /// </summary>
+        /// <param name="i2c_bus">I2C总线ID</param>
+        public SSD1306_128_64(int i2c_bus) : base(128, 64, i2c_bus)
+        {
+
+        }
+
+        public override void Initialize()
+        {
+
+            Command(SSD1306_DISPLAYOFF);            // 0xAE
+            Command(SSD1306_SETDISPLAYCLOCKDIV);    // 0xD5
+            Command(0x80);                          // 建议的比率 0x80
+            Command(SSD1306_SETMULTIPLEX);          //0xA8
+            Command(0x3F);
+            Command(SSD1306_SETDISPLAYOFFSET);      // 0xD3
+            Command(0x0);                           // 无偏移量
+            Command(SSD1306_SETSTARTLINE | 0x0);    // line #0
+            Command(SSD1306_CHARGEPUMP);            // 0x8D
+            if (this._vccstate == SSD1306_EXTERNALVCC)
+                Command(0x10);
+            else
+                Command(0x14);
+            Command(SSD1306_MEMORYMODE);            // 0x20
+            Command(0x00);                          // 0x0 act like ks0108
+            Command(SSD1306_SEGREMAP | 0x1);
+            Command(SSD1306_COMSCANDEC);
+            Command(SSD1306_SETCOMPINS);            // 0xDA
+            Command(0x12);
+            Command(SSD1306_SETCONTRAST);           // 0x81
+            if (this._vccstate == SSD1306_EXTERNALVCC)
+                Command(0x9F);
+            else
+                Command(0xCF);
+            Command(SSD1306_SETPRECHARGE);          // 0xd9
+            if (this._vccstate == SSD1306_EXTERNALVCC)
+                Command(0x22);
+            else
+                Command(0xF1);
+            Command(SSD1306_SETVCOMDETECT);         // 0xDB
+            Command(0x40);
+            Command(SSD1306_DISPLAYALLON_RESUME);   // 0xA4
+            Command(SSD1306_NORMALDISPLAY);         // 0xA6
+        }
+    }
+}
---
+++ Sang.IoT.SSD1306/SSD1306_Base.cs
@@ -0,0 +1,188 @@
+﻿using System.Device.I2c;
+
+namespace Sang.SSD1306
+{
+    public class SSD1306_Base : IDisposable
+    {
+        #region 常量
+        public const byte SSD1306_I2C_ADDRESS = 0x3C;    // 011110+SA0+RW - 0x3C or 0x3D
+        public const byte SSD1306_SETCONTRAST = 0x81;
+        public const byte SSD1306_DISPLAYALLON_RESUME = 0xA4;
+        public const byte SSD1306_DISPLAYALLON = 0xA5;
+        public const byte SSD1306_NORMALDISPLAY = 0xA6;
+        public const byte SSD1306_INVERTDISPLAY = 0xA7;
+        public const byte SSD1306_DISPLAYOFF = 0xAE;
+        public const byte SSD1306_DISPLAYON = 0xAF;
+        public const byte SSD1306_SETDISPLAYOFFSET = 0xD3;
+        public const byte SSD1306_SETCOMPINS = 0xDA;
+        public const byte SSD1306_SETVCOMDETECT = 0xDB;
+        public const byte SSD1306_SETDISPLAYCLOCKDIV = 0xD5;
+        public const byte SSD1306_SETPRECHARGE = 0xD9;
+        public const byte SSD1306_SETMULTIPLEX = 0xA8;
+        public const byte SSD1306_SETLOWCOLUMN = 0x00;
+        public const byte SSD1306_SETHIGHCOLUMN = 0x10;
+        public const byte SSD1306_SETSTARTLINE = 0x40;
+        public const byte SSD1306_MEMORYMODE = 0x20;
+        public const byte SSD1306_COLUMNADDR = 0x21;
+        public const byte SSD1306_PAGEADDR = 0x22;
+        public const byte SSD1306_COMSCANINC = 0xC0;
+        public const byte SSD1306_COMSCANDEC = 0xC8;
+        public const byte SSD1306_SEGREMAP = 0xA0;
+        public const byte SSD1306_CHARGEPUMP = 0x8D;
+        public const byte SSD1306_EXTERNALVCC = 0x1;
+        public const byte SSD1306_SWITCHCAPVCC = 0x2;
+        #endregion
+
+        #region 滚动常量
+        public const byte SSD1306_ACTIVATE_SCROLL = 0x2F;
+        public const byte SSD1306_DEACTIVATE_SCROLL = 0x2E;
+        public const byte SSD1306_SET_VERTICAL_SCROLL_AREA = 0xA3;
+        public const byte SSD1306_RIGHT_HORIZONTAL_SCROLL = 0x26;
+        public const byte SSD1306_LEFT_HORIZONTAL_SCROLL = 0x27;
+        public const byte SSD1306_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL = 0x29;
+        public const byte SSD1306_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL = 0x2A;
+        #endregion
+
+        /// <summary>
+        /// 屏幕宽度
+        /// </summary>
+        private int width;
+
+        /// <summary>
+        /// 屏幕高度
+        /// </summary>
+        private int height;
+
+        /// <summary>
+        /// I2C总线
+        /// </summary>
+        private readonly I2cBus _bus;
+
+        /// <summary>
+        /// I2C设备
+        /// </summary>
+        private readonly I2cDevice _i2c;
+
+
+        /// <summary>
+        /// 页数
+        /// 
+        /// 要想将数组数据显示到屏幕，就需要将数据存入，要用到了SSD1306的RAM
+        /// GDDRAM是一个位映射的静态RAM，保持要显示的位模式。
+        /// RAM大小为128x64位，RAM分为8页，从0页到7页，用于单色128x64点矩阵显示
+        /// https://blog.csdn.net/longgyue/article/details/121885956
+        /// 
+        /// </summary>
+        private int _pages;
+        private byte[] _buffer;
+
+        /// <summary>
+        /// vcc 状态
+        /// </summary>
+        public byte _vccstate;
+
+        /// <summary>
+        /// SSD1306_Base
+        /// </summary>
+        /// <param name="width">屏幕宽度</param>
+        /// <param name="height">屏幕高度</param>
+        /// <param name="i2c_bus">I2C总线ID</param>
+        /// <param name="i2c_address">I2C设备地址</param>
+        public SSD1306_Base(int width, int height, int i2c_bus, byte i2c_address = SSD1306_I2C_ADDRESS)
+        {
+            this.width = width;
+            this.height = height;
+            this._pages = (int)Math.Floor(height / 8.0);
+            this._buffer = new byte[width * this._pages];
+
+            this._bus = I2cBus.Create(i2c_bus);
+            this._i2c = _bus.CreateDevice(i2c_address);
+
+        }
+
+
+        /// <summary>
+        /// 针对不同分辨率设备的初始化
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public virtual void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 发送byte指令到显示器
+        /// </summary>
+        /// <param name="c"></param>
+        public void Command(byte c)
+        {
+            // Co = 0, DC = 0
+            _i2c.Write(new byte[] { 0x00, c });
+        }
+
+        /// <summary>
+        /// 发送data数据到显示器
+        /// </summary>
+        /// <param name="c"></param>
+        public void Data(byte[] c)
+        {
+            byte[] data = new byte[c.Length + 1];
+            data[0] = 0x40;
+            Array.Copy(c, 0, data, 1, c.Length);
+            _i2c.Write(c);
+        }
+
+
+        /// <summary>
+        /// 初始化库
+        /// </summary>
+        public void Begin(byte vccstate = SSD1306_SWITCHCAPVCC)
+        {
+            //保存vcc状态
+            this._vccstate = vccstate;
+            this.Initialize();
+            this.Command(SSD1306_DISPLAYON);
+        }
+
+        /// <summary>
+        /// 将buffer数据写入到显示器
+        /// </summary>
+        public void Display()
+        {
+            Command(SSD1306_COLUMNADDR);
+            Command(0);              // 列起始地址 (0 = reset)
+            Command((byte)(this.width - 1));  // 列结束地址
+            Command(SSD1306_PAGEADDR);
+            Command(0);              // 开始页 (0 = reset)
+            Command((byte)(this._pages - 1));  // 结束页
+            for (int i = 0; i < this._buffer.Length; i += 16)
+            {
+                Data(this._buffer[i..(i + 16)]);
+            }
+        }
+
+        /// <summary>
+        /// 清除buffer内容
+        /// </summary>
+        public void Clear()
+        {
+            this._buffer = new byte[width * this._pages];
+        }
+
+
+        /// <summary>
+        /// 释放资源
+        /// </summary>
+        public void Dispose()
+        {
+            if (this._i2c != null)
+            {
+                _i2c.Dispose();
+            }
+            if (this._bus != null)
+            {
+                _bus.Dispose();
+            }
+        }
+    }
+}
