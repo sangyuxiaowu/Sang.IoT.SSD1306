44d59781a120cb2c7f868bec58f35ec9b522ea16
桑榆肖物 <sang93@qq.com>
2025-02-09 00:32:44 +0800

    feat: 添加ImageTest方法，支持图像绘制并保存为PNG文件

--- Sang.IoT.SSD1306.Test/Program.cs
+++ Sang.IoT.SSD1306.Test/Program.cs
@@ -5,6 +5,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        ImageTest();
         BaseTest();
     }
 
@@ -36,6 +37,27 @@ internal class Program
             }
 
             oled.Display();
+        }
+    }
+
+    private static void ImageTest()
+    {
+        using (var bitmap = new SKBitmap(128, 64, true))
+        {
+            SKCanvas canvas = new SKCanvas(bitmap);
+            SKPaint paint = new SKPaint()
+            {
+                Color = new SKColor(255, 255, 255),
+                StrokeWidth = 1, //画笔宽度
+                Style = SKPaintStyle.Fill,
+            };
+            SKFont font = new SKFont(SKTypeface.Default, 13);
+
+            canvas.DrawText("Sang.IoT.SSD1306 ", 0, 13, font, paint);
+            font.Size = 30;
+            canvas.DrawText("桑榆肖物 ", 0, 50, font, paint);
+            using (FileStream fs = new FileStream("tmp.png", FileMode.Create))
+                bitmap.Encode(SKEncodedImageFormat.Png, 100).SaveTo(fs);
         }
     }
 }
